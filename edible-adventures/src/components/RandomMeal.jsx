787c3f3c9bc6e60f1d6b87580d63308d6b8755d0
M10mo <mohdkanoongo@gmail.com>
2024-03-22 16:03:32 -0400

    fetching and rendering random ingredients

--- edible-adventures/src/components/RandomMeal.jsx
+++ edible-adventures/src/components/RandomMeal.jsx
@@ -9,6 +9,22 @@ const RandomMeal = () => {
     setMeal(randomMeal);
   };
 
+  const getIngredientsList = (meal) => {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}`];
+      const measure = meal[`strMeasure${i}`];
+      if (ingredient) {
+        ingredients.push(
+          <li key={i}>
+            {ingredient} - {measure}
+          </li>
+        );
+      }
+    }
+    return ingredients;
+  };
+
   return (
     <div>
       <h2>Random Meal</h2>
@@ -17,7 +33,9 @@ const RandomMeal = () => {
         <div>
           <h3>{meal.strMeal}</h3>
           <img src={meal.strMealThumb} alt={meal.strMeal} />
-          {/* <ul>{getIngredientsList(meal)}</ul> */}
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            {getIngredientsList(meal)}
+          </ul>
           <p>{meal.strInstructions}</p>
         </div>
       )}
